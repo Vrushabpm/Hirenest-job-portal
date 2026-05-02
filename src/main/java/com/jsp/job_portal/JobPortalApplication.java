ea242788cf459c8a065c5837c364672920c3e8cc
vrushabpm <vrushabpm@gmail.com>
2026-05-02 22:42:34 +0530

    Initial commit - HireNest Job Portal

---
+++ src/main/java/com/jsp/job_portal/JobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.job_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
+public class JobPortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalApplication.class, args);
+    }
+}
